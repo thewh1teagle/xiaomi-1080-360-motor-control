bff1ab89fbb83601fe78da96afab944a7cacfb7a
Zadkiel <hello@zadkiel.fr>
2020-02-24 02:00:07 +0100

    fix: lint pull_libs

--- pull_libs.sh
+++ pull_libs.sh
@@ -1,13 +1,16 @@
-#!/usr/bin/bash
+#!/bin/sh
 
-if [ "$1" == "" ] || [ "$2" == "" ] || [ "$3" == "" ] 
-then
-    echo "Usage: ./pull_libs <IP> <FTP USER> <PASS>"
+set -e
+
+if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
+    echo "Usage: $0 <HOST> <USERNAME> <PASSWORD>"
+    echo
+    echo "Pull libraries from remote camera over FTP."
     exit 1
 fi
 
-IP=$1 USER=$2 PASS=$3
+HOST=$1 USERNAME=$2 PASSWORD=$3
 
-curl -u $USER:$PASS 'ftp://'$IP'/mnt/sdcard/bin/file' -o file
+curl -u "$USERNAME:$PASSWORD" "ftp://$HOST/mnt/sdcard/bin/file' -o file
 
-echo "finished!."
+echo "finished!"
