853252f05bca2f5316f1a08b291cbe336a7ee0a0
thewh1teagle <61390950+thewh1teagle@users.noreply.github.com>
2020-02-24 22:09:09 +0200

    big change ( Not finished yet )

--- pull_libs.sh
+++ pull_libs.sh
@@ -1,46 +1,32 @@
-#!/bin/sh
+#!/bin/bash
 
-set -e
+# set -e
+set -x
 
 if [ -z "$1" ] || [ -z "$2" ] || [ -z "$3" ] ; then
-    echo "Usage: $0 <HOST> <USERNAME> <PASSWORD>"
+    echo "Usage: $0 <HOST> <USERNAME> <PASSWORD> <OUTPATH>"
     echo
     echo "Pull libraries from remote camera over FTP."
     exit 1
 fi
 
 HOST="$1" USERNAME="$2" PASSWORD="$3"
+PATH="$3"
+
+fetch_libs() { # params : ftp folder path, libs strings array
+    path=$1 arr=$2
+    for LIB in "${"$arr"[@]}"
+    do
+        curl -u "$USERNAME:$PASSWORD" "ftp://${HOST}$1/$LIB" --output "./lib/$LIB" 
+    done
+}
 
 
-curl -u "$USERNAME:$PASSWORD" "ftp://$HOST/mnt/sdcard/bin/file' -o file
-
-echo "finished!"
-
-
-declare -a StringArray=("libdevice_kit.so.0.0.1" "libmortox.so.0.0.0" "libboardav.so.1.0.0" "libmortox_share.so.0.0.0" "libmiio_util.so.0.0.1" "libmortoxev.so.0.0.0" "libmi_isp.so" "libMTE_LINUX.so" )
+declare -a mnt_libs=("libdevice_kit.so.0.0.1" "libmortox.so.0.0.0" "libboardav.so.1.0.0" \
+                    "libmortox_share.so.0.0.0" "libmiio_util.so.0.0.1" "libmortoxev.so.0.0.0" \
+                    "libmi_isp.so" "libMTE_LINUX.so")
 
 
-for val in ${StringArray[@]}; do
-   echo $val
-done
+declare -a root_libs=("")
 
-# curl -u $USER:$PASS 'ftp://'$IP'/mnt/sdcard/bin/file' -o file
-
-# echo "finished!."
-
-
-# /mnt/data/lib
-# libdevice_kit.so.0.0.1
-# libmiio_util.so.0.0.1
-# libboardav.so.1.0.0
-# libmortox.so.0.0.0
-# libmortox_share.so.0.0.0
-# libmortoxev.so.0.0.0
-# libmi_isp.so
-# libMTE_LINUX.so
-
-# /lib
-# libOMX_BELA.so
-# libOMX_AVQE_A.so
-# libpthread-2.25.so
-# librt-2.25.so
+fetch_libs "/mnt/data/lib" mnt_libs
